1bc8ece65a84e87b7e07bf8c4da073e439e2adcc
yinshuangfei <yinshuangfei007@163.com>
2022-07-12 23:35:16 +0800

    add Rust, add bochs

--- Rust/test/test1/src/main.rs
+++ Rust/test/test1/src/main.rs
@@ -1,12 +1,31 @@
-use std::io; // prelude
-// https://github.com/mstorsjo/llvm-mingw/releases
+use std::io;            // prelude
+use std::cmp::Ordering; // 枚举类型
+use rand::Rng;          // trait
 
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, this is a secret number game!");
 
-    let mut guess = String::new();
+    let secret_num = rand::thread_rng().gen_range(1, 101);
 
-    io::stdin().read_line(&mut guess).expect("read error.");
-
-    println!("Your number: {}", guess);
+    loop {
+        println!("please give your number");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("read error.");
+    
+        // shadow old guess
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+    
+        println!("Your number: {}", guess);
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You get it!");
+                break;
+            }
+        }
+    }
 }
---
+++ myOS/README.md
@@ -0,0 +1,8 @@
+# This is a os project for myself.
+# It was small.
+
+
+
+# How to use bochs
+1.安装 Bochs-2.3.6.exe；
+2.制作 bochdsrc.txt 配置文件；
